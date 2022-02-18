dfc8ecd7cf48c3654c222e71939eb70f8e705082
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2022-02-19 01:03:43 +0330

    Added dark mode support

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -13,21 +13,54 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' ||(!('color-theme' in localStorage) &&window.matchMedia('(prefers-color-scheme: dark)').matches)) {document.documentElement.classList.add('dark');} else {document.documentElement.classList.remove('dark');}
+    </script>
 </head>
 
-<body class="antialiased dark:bg-gray-900">
-    <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<body class="antialiased dark:bg-slate-900">
+    <button
+    id="theme-toggle"
+    type="button"
+    class="text-slate-500  dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-200 dark:focus:ring-slate-700 rounded-lg text-sm p-2 m-4"
+  >
+    <svg
+      id="theme-toggle-dark-icon"
+      class="w-5 h-5 hidden"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+      ></path>
+    </svg>
+    <svg
+      id="theme-toggle-light-icon"
+      class="w-5 h-5 hidden"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </button>
+    <div class="relative flex items-top justify-center min-h-screen dark:bg-slate-900 sm:items-center py-4 sm:pt-0">
 
 
 
         <div class="w-full md:w-5/6 lg:w-4/6 mx-auto sm:px-6 lg:px-8">
-            <div class="p-6 mt-8 w-full bg-white dark:bg-gray-800 overflow-hidden">
-                <div class="mt-2 text-gray-900 dark:text-gray-400">
+            <div class="p-6 mt-8 w-full bg-white dark:bg-slate-800 overflow-hidden">
+                <div class="mt-2 text-slate-900  dark:text-slate-300">
                     <h1 class="font-bold text-lg md:text-2xl">تخته آزاد</h1>
 
                     <div class="flex gap-4 my-6">
-                        <a href="{{ route('about') }}" class="underline hover:text-gray-700">درباره تخته آزاد</a>
-                        <a href="{{ route('howTo') }}" class="underline hover:text-gray-700">چگونگی استفاده</a>
+                        <a href="{{ route('about') }}" class="underline hover:text-slate-700 dark:hover:text-slate-100">درباره تخته آزاد</a>
+                        <a href="{{ route('howTo') }}" class="underline hover:text-slate-700 dark:hover:text-slate-100 ">چگونگی استفاده</a>
                     </div>
 
                     <h2 class="font-bold mb-4 text-lg md:text-xl">
@@ -47,13 +80,13 @@
                         @csrf
                     <div class="md:flex md:flex-col md:items-start mb-3">
                         <div class="w-full">
-                            <label class="block text-black font-bold md:text-right mb-1" for="content">
+                            <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="content">
                                 محتوا
                             </label>
                         </div>
                         <div class="w-full">
                             <textarea name="content"
-                                class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                 id="" cols="80" rows="10">{{ old('content', $post->content) }}</textarea>
                                 @error('content')
                                 <div class="text-red-700 underline alert alert-danger">{{ $message }}</div>
@@ -67,14 +100,14 @@
 
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="slug">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="slug">
                                     آدرس صفحه (حروف، اعداد، خط فاصله قابل قبول است و بعد از openboard.ir/ قرار می‌گیرد)
                                 </label>
                             </div>
                             <div class="w-full">
                                 <input name="slug"
                                 value="{{ old('slug', $post->slug) }}"
-                                    class=" bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class=" bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" />
                                     @error('slug')
                                     <div class="text-red-700 underline alert alert-danger">{{ $message }}</div>
@@ -84,54 +117,54 @@
 
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="code">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="code">
                                     کد ویرایش (برای ویرایش صفحه لازم خواهد بود)
                                 </label>
                             </div>
                             <div class="w-full">
                                 <input name="code"
-                                    class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" />
                             </div>
                         </div>
 
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="title">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="title">
                                     عنوان صفحه
                                 </label>
                             </div>
                             <div class="w-full">
                                 <input name="title"
                                 value="{{ old('title', $post->title) }}"
-                                    class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" />
                             </div>
                         </div>
 
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="author_name">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="author_name">
                                     نویسنده
                                 </label>
                             </div>
                             <div class="w-full">
                                 <input name="author_name"
                                 value="{{ old('author_name', $post->author_name) }}"
-                                    class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" />
                             </div>
                         </div>
 
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="description">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="description">
                                     خلاصه
                                 </label>
                             </div>
                             <div class="w-full">
                                 <textarea name="description"
-                                    class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" cols="80" rows="5">{{ old('description', $post->description) }}</textarea>
                             </div>
                         </div>
@@ -139,14 +172,14 @@
                         @if($post->exists)
                         <div class="md:flex md:flex-col md:items-start mb-3">
                             <div class="w-full">
-                                <label class="block text-black font-bold md:text-right mb-1" for="inline-full-name">
+                                <label class="block text-black font-bold md:text-right mb-1 dark:text-slate-200" for="inline-full-name">
                                     کد ویرایش صفحه
                                 </label>
                             </div>
                             <div class="w-full">
                                 <input name="submitted_code"
                                 value="{{ old('submitted_code', $post->submitted_code) }}"
-                                    class="bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    class="bg-slate-100 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-700 leading-tight focus:outline-none focus:bg-white focus:border-slate-500"
                                     id="" />
                                     @error('submitted_code')
                                     <div class="text-red-700 underline alert alert-danger">{{ $message }}</div>
@@ -158,12 +191,12 @@
 
                     <div class="flex gap-4 justify-end">
                         <button onclick="advancedButton()"
-                            class="shadow bg-gray-700 text-sm hover:bg-gray-900 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded"
+                            class="shadow bg-slate-700 text-sm hover:bg-slate-900 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded"
                             type="button">
                             تنظیمات پیشرفته
                         </button>
                         <button
-                            class="shadow bg-gray-700 text-sm hover:bg-gray-900 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded"
+                            class="shadow bg-slate-700 text-sm hover:bg-slate-900 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded"
                             type="submit">
                             ذخیره
                         </button>
@@ -180,6 +213,47 @@
         var element = document.getElementById("advanced");
         element.classList.toggle("hidden");
     }
+
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
 </script>
 
 </html>
--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -13,20 +13,53 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <script>
+        if (localStorage.getItem('color-theme') === 'dark' ||(!('color-theme' in localStorage) &&window.matchMedia('(prefers-color-scheme: dark)').matches)) {document.documentElement.classList.add('dark');} else {document.documentElement.classList.remove('dark');}
+    </script>
 </head>
 
-<body class="antialiased dark:bg-gray-900">
-    <div class="relative flex items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<body class="antialiased dark:bg-slate-900">
+    <button
+    id="theme-toggle"
+    type="button"
+    class="text-slate-500  dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-700 focus:outline-none focus:ring-4 focus:ring-slate-200 dark:focus:ring-slate-700 rounded-lg text-sm p-2 m-4"
+  >
+    <svg
+      id="theme-toggle-dark-icon"
+      class="w-5 h-5 hidden"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+      ></path>
+    </svg>
+    <svg
+      id="theme-toggle-light-icon"
+      class="w-5 h-5 hidden"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </button>
+    <div class="relative flex items-top justify-center min-h-screen dark:bg-slate-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
-                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-black underline">Home</a>
+                    <a href="{{ url('/home') }}" class="text-sm text-slate-700 dark:text-black underline">Home</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-black underline">Log in</a>
+                    <a href="{{ route('login') }}" class="text-sm text-slate-700 dark:text-black underline">Log in</a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-black underline">Register</a>
+                            class="ml-4 text-sm text-slate-700 dark:text-black underline">Register</a>
                     @endif
                 @endauth
             </div>
@@ -36,13 +69,13 @@
 
 
         <div class="w-full md:w-5/6 lg:w-4/6 mx-auto sm:px-6 lg:px-8">
-            <div class="p-6 mt-8 w-full bg-white dark:bg-gray-800 overflow-hidden">
-                <div class="mt-2 text-gray-900 dark:text-gray-400">
+            <div class="p-6 mt-8 w-full bg-white dark:bg-slate-800 overflow-hidden">
+                <div class="mt-2 text-slate-900  dark:text-slate-300">
                     <h1 class="font-bold text-lg md:text-2xl">تخته آزاد</h1>
 
                     <div class="flex gap-4 my-6">
-                        <a href="#" class="underline hover:text-gray-700">درباره تخته آزاد</a>
-                        <a href="#" class="underline hover:text-gray-700">چگونگی استفاده</a>
+                        <a href="#" class="underline hover:text-slate-700 dark:hover:text-slate-100">درباره تخته آزاد</a>
+                        <a href="#" class="underline hover:text-slate-700 dark:hover:text-slate-100">چگونگی استفاده</a>
                     </div>
 
                     <h2 class="font-bold mb-4 text-lg md:text-xl">صفحه شما حاضر است</h2>
@@ -75,6 +108,47 @@
         var element = document.getElementById("advanced");
         element.classList.toggle("hidden");
     }
+
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
 </script>
 
 </html>
