a73ab62b56f86b198a5091495b481adcf218318c
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2022-05-04 00:45:57 +0430

    Added font-display swap to font family

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -5,14 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="The most minimalist content management system for Persian users.">
-    
+
     <title>تخته آزاد</title>
 
     <!-- Fonts -->
-    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet" defer>
+    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" defer>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <script>
         if (localStorage.getItem('color-theme') === 'dark' ||(!('color-theme' in localStorage) &&window.matchMedia('(prefers-color-scheme: dark)').matches)) {document.documentElement.classList.add('dark');} else {document.documentElement.classList.remove('dark');}
