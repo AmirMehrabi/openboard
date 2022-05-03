59806f2a316d4619e5d294a32e5e71d39be100bf
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2022-05-04 00:40:55 +0430

    Added a description meta data

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The most minimalist content management system for Persian users.">
 
     <title>تخته آزاد</title>
 
