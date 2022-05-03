3abc28474a9581f38861f6fd5cd49f7fa1481138
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2022-05-04 00:37:24 +0430

    Fixed some issues with stylesheets

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -8,10 +8,10 @@
     <title>تخته آزاد</title>
 
     <!-- Fonts -->
-    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fonts.css') }}" rel="stylesheet" defer>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" defer>
 
     <script>
         if (localStorage.getItem('color-theme') === 'dark' ||(!('color-theme' in localStorage) &&window.matchMedia('(prefers-color-scheme: dark)').matches)) {document.documentElement.classList.add('dark');} else {document.documentElement.classList.remove('dark');}
